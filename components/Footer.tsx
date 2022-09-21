5a2b55f64db5f8ec26552b77746e02fb7b242ad2
mangage <114001456+mangage@users.noreply.github.com>
2022-09-22 02:00:08 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -33,7 +33,20 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2022 {config.author}</div>
+      <div className={styles.copyright}>
+        <div
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
+          <a href='https://hits.seeyoufarm.com'>
+            <img
+              style={{ display: 'block', margin: '0px auto' }}
+              src='https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fmangzige.com&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=true'
+            />
+          </a>
+          <div>Copyright 2022 {config.author}</div>
 
       <div className={styles.settings}>
         {hasMounted && (
